f1d34f709ba6db10f98dcff1c60d3ce144dc1f73
Andy Yeung <andyyeung122@gmail.com>
2022-01-03 18:01:44 -0500

    added input validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,19 +2,43 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
   const [userName, setUserName] = useState('');
   const [age, setAge] = useState('');
+  const [isValid, setIsValid] = useState(true);
+  const [errorMsg, setErrorMsg] = useState();
 
   const onAddUserHandler = (event) => {
     event.preventDefault();
+
+    if (userName.trim().length === 0 || age.trim().length === 0) {
+      setErrorMsg({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
+      setIsValid(false);
+      return;
+    }
+    if (+age < 1 || isNaN(age)) {
+      setErrorMsg({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
+      setIsValid(false);
+      return;
+    }
 
     console.log(userName, age);
 
     props.onAddUser(userName, age);
     setUserName('');
     setAge('');
+  };
+
+  const onConfirmHandler = () => {
+    setIsValid(true);
   };
 
   const onUsernameChange = (event) => {
@@ -27,6 +51,13 @@ const AddUser = (props) => {
 
   return (
     <Card className={styles.input}>
+      {!isValid && (
+        <ErrorModal
+          onConfirm={onConfirmHandler}
+          title={errorMsg.title}
+          message={errorMsg.message}
+        />
+      )}
       <form onSubmit={onAddUserHandler}>
         <label>Username</label>
         <input
